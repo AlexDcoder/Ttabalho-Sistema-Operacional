c232d5c13346b2e162b452c707953ebb4108ff55
Alex <alexandrepinto586@gmail.com>
2024-03-19 10:44:07 -0300

    alterações no código e no readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Trabalhos de Sistemas Operacionais
+# <h1><i>Trabalhos de Sistemas Operacionais</i></h1>
+
+
 <h2>Membros</h2>
+<h2>Contextualização do Primeiro Traballho</h2>
+<p>O trabalho de implementação consistirá de uma implementação do algoritmo de escalonamento ROUND-ROBIN. Vocês podem utilizar como base, a implementação do algoritmo "primeiro a chegar, primeiro a ser servido" como uma base. Lembrando que este último foi implementado em casa e consta na seção de códigos.</p>
+</p>
+<br>
+
+<h2>Contextualização do Segundo Traballho</h2>
+<p>O trabalho consistirá da pesquisa de 2 das 5 técnicas de exclusão mútua apresentadas no slide 5 & 6.
+
+As técnicas são:
+
+<ul>
+<li>Desabilitação de  interrupções</li>
+<li>Variáveis lock(trava)</li>
+<li>Alternância Explícita</li>
+<li>Solução de Peterson</li>
+<li>Instrução TSL</li>
+</ul>
+</p>
+<br>
+<h2>Técnicas escolhidas</h2>
+<ol>
+<li><h2>Técnicas escolhidas</h2></li>
+<p><p>
+<li><h2>Técnicas escolhidas</h2></li>
+<p></p>
+</ol>
+<br>
+<h2>Referências</h2>
+
--- implementacao.c
+++ implementacao.c
@@ -5,6 +5,7 @@ typedef enum {
     PRONTO,
     EXECUTANDO,
     BLOQUEADO,
+    FINALIZADO,
 } EstadoProcesso;
 
 typedef struct {
@@ -15,12 +16,49 @@ typedef struct {
 
 // Criando n projetos de vários tipos
 
+void gerando_processos(Processo process[], int quantity){
+    for (int i = 0; i < quantity; i++)
+    {
+       process[i].id = i;
+       process[i].tempo_de_execucao = (50 + (50 * i));        
+       process[i].estado = PRONTO;
+    }
+    
+}
+
 // Criação do algorimto round robin
+void Round_Robin(int quantum, Processo some_process[], int quantidade_processos, int processo_atual){
+    if (some_process[atual].estado == PRONTO)
+    {
+        some_process[atual].estado = EXECUTANDO
+        printf("Processo %d está em EXECUÇÃO\n")
+        return Round_Robin(quantum, some_process, quantidade_processos, processo_atual);
+    }
+
+    if (some_process[atual].estado == EXECUTANDO)
+    {
+        some_process[atual].tempo_de_execucao -= quantum;
+
+        if some_process[atual].tempo_de_execucao <= 0{
+            printf("Processo %d foi FINALIZADO\n")
+            return Round_Robin(quantum, some_process, quantidade_processos, processo_atual);
+        }
+    }
+    
+    if (some_process[atual].estado == FINALIZADO)
+    {
+        
+    }
+    
+    
+}
 
 
 int main()
 {
-    Processo p;
-    
+    Processo alguns_processos[5];
+    gerando_processos(alguns_processos, 5);
+    printf("INICIANDO ROUND ROBIN\n");
+    Round_Robin(100, alguns_processos, sizeof(alguns_processos)/sizeof(alguns_processos[0]), 0);
     return 0;
 }
