00a9611f6e5963f5b34772829695a21737cafe23
Alex <alexandrepinto586@gmail.com>
2024-03-27 09:30:32 -0300

    envio final, alterações finais no readme e na implementação

--- README.md
+++ README.md
@@ -3,16 +3,21 @@
 
 ## Membros
 ## Contextualização do Primeiro Traballho
-*O trabalho de implementação consistirá de uma implementação do algoritmo de escalonamento ROUND-ROBIN. Vocês podem utilizar como base, a implementação do algoritmo "primeiro a chegar, primeiro a ser servido" como uma base. Lembrando que este último foi implementado em casa e consta na seção de códigos.*
+O trabalho de implementação consistirá de uma implementação do algoritmo de escalonamento ROUND-ROBIN. Vocês podem utilizar como base, a implementação do algoritmo "primeiro a chegar, primeiro a ser servido" como uma base. Lembrando que este último foi implementado em casa e consta na seção de códigos.
 
 ## Contextualização do Segundo Traballho
-*O trabalho consistirá da pesquisa de 2 das 5 técnicas de exclusão mútua apresentadas no slide 5 & 6.* 
-*As técnicas são:*
-**Desabilitação de  interrupções*
-**Variáveis lock(trava)*
-**Alternância Explícitas*
-**Solução de Peterson*
-**Desabilitação de  interrupçõesInstrução TSL*
+O trabalho consistirá da pesquisa de 2 das 5 técnicas de exclusão mútua apresentadas no slide 5 & 6. 
+As técnicas são:
+
+*Desabilitação de  interrupções
+
+*Variáveis lock(trava)
+
+*Alternância Explícitas
+
+*Solução de Peterson
+
+*Desabilitação de  interrupçõesInstrução TSL
 
 ## Técnicas Escolhidas
 
@@ -21,17 +26,29 @@
 algum processo em uma região crítica*  
 *Se lock == 0 não há processo na região
 crítica.*   
-*Se lock == 1 há processo na região crítica.* 
+Se lock == 1 há processo na região crítica. 
 
-*A ideia é:*  
+A ideia é:  
 *Se o seu processo quer entrar na
 região crítica ele deve ler lock e só poderá
 entrar quando lock == 0, quando ele entrar deve fazer lock = 1*
 
-### Técnica  2
-*Descrição 2*
+### Desabilitar interrupções
+*- Ao entrar na região crítica ficam desabilitadas
+as interrupções*
+
+- Evita que ocorra troca de contexto por tempo.
+
+*- Em um sistema multiprocessado pode ser
+complexo ou impossível desabilitar a
+interrupção em todos os processadores*
+
+*- Entregar o controle das interrupções ao usuário
+pode ser perigoso se o processo não habilitar
+novamente estas interrupções.*
+
+
 # Referências:
 * [Aula07.pdf
 ](https://docente.ifrn.edu.br/tadeuferreira/disciplinas/2016.1/sistemas-operacionais/Aula07.pdf).
 * [Markdown Live Preview](https://markdownlivepreview.com/).
-
--- implementacao.c
+++ implementacao.c
@@ -1,71 +1,56 @@
 #include <stdio.h>
-#include <Windows.h>
 
 // Criação do conceito de Processo e seus Estados
-typedef enum
-{
+typedef enum {
     PRONTO,
     EXECUTANDO,
     BLOQUEADO,
     FINALIZADO,
 } EstadoProcesso;
 
-typedef struct
-{
+typedef struct EstruturaProcesso {
     int id;
     int tempo_de_execucao;
     EstadoProcesso estado;
 } Processo;
 
 // Criando n processos de vários tempos
-
-void gerando_processos(Processo process[], int quantity)
-{
-    for (int i = 0; i < quantity; i++)
-    {
+void gerando_processos(Processo process[], int quantity) {
+    for (int i = 0; i < quantity; i++) {
         process[i].id = i;
         process[i].tempo_de_execucao = (50 + (50 * i));
         process[i].estado = PRONTO;
     }
 }
 
-// Remover processo
-void removendo_proccessos(Processo process[], int index)
-{
-    // remover processo da array de processo
-}
-
-// Criação do algorimto round robin
-void Round_Robin(int quantum, Processo lista_de_processos[], int quantidade_processos)
-{
-    for (int i = 0; i < quantidade_processos; i++)
-    {
-        if(lista_de_processos[i].estado == PRONTO){
+// Criação do algoritmo round robin
+void Round_Robin(int quantum, Processo lista_de_processos[], int quantidade_processos, int cont) {
+    for (int i = 0; i < quantidade_processos; i++) {
+        if (lista_de_processos[i].estado == PRONTO) {
             printf("Processo %d em EXECUÇÃO\n", i + 1);
             lista_de_processos[i].estado = EXECUTANDO;
             lista_de_processos[i].tempo_de_execucao = lista_de_processos[i].tempo_de_execucao - quantum;
+            if (lista_de_processos[i].tempo_de_execucao <= 0) {
+                // Remover processo
+                printf("Processo %d CONCLUÍDO\n", i + 1);
+                lista_de_processos[i].estado = FINALIZADO;
+                cont += 1;
+            } else {
+                lista_de_processos[i].estado = PRONTO;
+            }
         }
-        Sleep(10);
-        if (lista_de_processos[i].tempo_de_execucao <= 0)
-        {
-            printf("Processo %d em CONCLUIDO\n", i + 1);
-            lista_de_processos[i].estado = FINALIZADO;
-            // TODO : remover o elemento da lista e atualizar a quantidade de processos
-        }
-        else
-        {
-            lista_de_processos[i].estado = PRONTO;
-        }
-        // criar o ciclo da lista com recursão
+    }
+    if(cont < quantidade_processos) {
+        Round_Robin(quantum, lista_de_processos, quantidade_processos, cont);
     }
 }
 
-int main()
-{
+int main() {
     Processo alguns_processos[5];
     gerando_processos(alguns_processos, 5);
     printf("INICIANDO ROUND ROBIN\n");
-    Round_Robin(100, alguns_processos, sizeof(alguns_processos) / sizeof(alguns_processos[0]));
+    //Round_Robin(100, alguns_processos, sizeof(alguns_processos) / sizeof(alguns_processos[0]));
+    Round_Robin(100, alguns_processos, 5, 0);
     printf("FINALIZANDO ROUND ROBIN\n");
     return 0;
 }
